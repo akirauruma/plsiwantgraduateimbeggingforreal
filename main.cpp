ebc9d16ba0b7ac33a114cb63b546f3dec15946ca
Victor Ternolutski <99807453+akirauruma@users.noreply.github.com>
2025-09-17 15:14:53 +0300

    Update main.cpp
    Добавлена функция для подсчета среднего возраста

--- main.cpp
+++ main.cpp
@@ -25,15 +25,26 @@ void addStudent(std::vector<Student>& database) {
     std::cout << "Студент добавлен в базу данных.\n";
 }
 
-// Функция для вывода всех студентов из базы данных
+// Функция для вывода всех студентов из базы данных и среднего возраста
 void displayStudents(const std::vector<Student>& database) {
     std::cout << "Список студентов:\n";
+
+    if (database.empty()) {
+        std::cout << "База данных пуста.\n";
+        return;
+    }
+
+    int sumAges = 0;
     for (const Student& student : database) {
         std::cout << "Имя: " << student.name << "\n";
         std::cout << "Возраст: " << student.age << "\n";
         std::cout << "Специальность: " << student.major << "\n";
         std::cout << "Средний балл: " << student.gpa << "\n\n";
+        sumAges += student.age;
     }
+
+    double averageAge = static_cast<double>(sumAges) / database.size();
+    std::cout << "Средний возраст всех студентов: " << averageAge << "\n";
 }
 
 int main() {
